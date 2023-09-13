85216f26d8da24b67e4f90618bd79ccf1b85a8e7
NinjaKnight1 <christoffer.dam.hansen@gmail.com>
2023-09-13 08:56:49 +0200

    Bit array and time

--- code_and_samples/main.c
+++ code_and_samples/main.c
@@ -6,18 +6,16 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "cbmp.h"
 
-#define threashold 260
-
-  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char control_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 
 
-//Function to invert the image to black and white
-void toGreyScale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+
+//Declaring the 
+clock_t start, end;
+double cpu_time_used;
+
 //Declaring the array to store the image (unsigned char = unsigned 8 bit)
 unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
@@ -36,7 +34,7 @@ void toBlackWhite(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]
       for (int c = 0; c < BMP_CHANNELS; c++){ //Sums up the rgb value
         sum += input_image[x][y][c];
       }
-      if (sum >= threashold){ //checks with higher treshold instead of averaging (is that a word?) values 
+      if (sum >= threshold){ //checks with higher treshold instead of averaging (is that a word?) values 
         for(int i = 0; i < 3; i++){
           output_image[x][y][i] = 255;
         }
@@ -185,6 +183,8 @@ int main(int argc, char** argv)
         }
       }
     }
+
+  start = clock();
   while (erode(control_image,output_image)){ //while something was eroded
     count = countCells(output_image,count,coords,searchRadius);
     
@@ -196,6 +196,7 @@ int main(int argc, char** argv)
       }
     }
   }
+  end = clock();
 
   printf("final count %d",count);
 
@@ -226,6 +227,11 @@ int main(int argc, char** argv)
         }}
     }
   }
+  
   write_bitmap(output_image,argv[2]);
+  cpu_time_used = (double) (end - start);
+
+  printf("Total time: %f ms\n", cpu_time_used * 1000 / CLOCKS_PER_SEC);
+
   return 0;
 }
