d42be9bc996994d290a5025c2b709d436cdd9346
DeMoManique <frederik.hvarregaard.andersen@gmail.com>
2023-09-22 16:55:29 +0200

    implemented otsu

---
+++ code_and_samples/BinaryScaling.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "cbmp.h"
+
+//Otsu
+unsigned char otsuThreshold(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+    int pixels = BMP_HEIGTH*BMP_WIDTH;
+    int histogram[256] = {0};
+
+    for(int x = 0; x < BMP_WIDTH; x++){
+        for(int y = 0; y < BMP_HEIGTH; y++){
+            histogram[(image[x][y][0]+image[x][y][1]+image[x][y][2])/3]++;
+        }
+    }
+    for(int i = 0; i < 256; i++){ //TODO Remove this print function
+        printf("%d \n", histogram[i]);
+    }
+
+    unsigned int intensity = 0; //Can be thought of as image intensity
+    for(int i = 0; i < 256; i++){
+        intensity += i*histogram[i];
+    } 
+
+    double BackgroundIntensity = 0.0;
+    int weightBackground = 0;   //The amount of pixels in background
+    int weightForeground = 0;   //----------||----------- foreground
+    double meanBackround = 0.0;
+    double meanForeground = 0.0;
+    double variance = 0.0;
+    double maxVariance = 0.0;   //keeps track of the best variance
+    int threshold = 0;          //----------||----------- threshold
+
+    for(int i = 0; i < 256; i++){
+        weightBackground += histogram[i];
+        if(weightBackground == 0) continue; //If there isnt any pixels in background there is no reason to calculate threshold
+
+        weightForeground = pixels-weightBackground;
+        if(weightForeground == 0) break; //If there are no pixels left in foreground, best threshold should already be found
+
+        BackgroundIntensity += (double)(i*histogram[i]);
+
+        meanBackround = BackgroundIntensity/weightBackground;
+        meanForeground = (intensity-BackgroundIntensity)/weightForeground;
+
+        variance = (double)weightBackground*(double)weightForeground*(meanBackround-meanForeground)*(meanBackround-meanForeground);
+
+        if (variance > maxVariance){
+            maxVariance = variance;
+            threshold = i;
+        }
+    }
+    return(threshold);
+}
+
+
+
+
+//Convert pixels to bits
+
+//convert bits to pixels
--- code_and_samples/main.c
+++ code_and_samples/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <time.h>
 #include "cbmp.h"
+#include "BinaryScaling.c"
 
 // Declaring the
 clock_t start, end;
@@ -120,85 +121,6 @@ char erodeBitArray(unsigned char output_image_bit[952][119],
   return boolean;
 }
 
-int countCellsBit(unsigned char image[952][119],
-                  int count)
-{
-  char bit;
-  for (int x = 1; x < 952; x++)
-  {
-    for (int y = 0; y < 119; y++)
-    {
-      // checks if one of the 8 bits is true
-      if (image[x][y])
-      {
-        bit = 7;
-        while (bit >= 0)
-        {
-          // finds the first true bit, from left to right
-          if ((image[x][y] >> bit) & 0x01)
-          {
-            // If it is to close to the top
-            if ((y == 0 && bit > 3) && 
-                (((x > 3) && ((image[x - 3][y] & 0x7F) >> (bit - 4))) || 
-                ((x <= 946) && ((image[x + 4][y] & 0x7F) >> (bit - 4))) || 
-                checkTheXAksis(image, x, y, bit)))
-            {
-            }
-            // If it is to close to the bottom
-            else if ((y == 118 && bit < 5) && 
-                (((x > 3) && ((image[x - 3][y] & 0xFE) << (4 - bit))) || 
-                ((x <= 946) && ((image[x + 4][y] & 0xFE) << (4 - bit))) || 
-                checkTheXAksis(image, x, y, bit)))
-            {
-            }
-            // in the middle of the char
-            else if (bit == 4)
-            {
-              if (((x > 3) && image[x - 3][y] || (x <= 946) && image[x + 4][y]) || checkTheXAksis(image, x, y, 0x10))
-              {
-              }
-              else
-              {
-                count++;
-                for (int i = -3; i <= 4; i++)
-                {
-                  image[x + i][y] = 0;
-                }
-              }
-            }
-            else if (bit > 4)
-            {
-              if (((x > 3) && ((image[x - 3][y] >> (bit - 4)) || (image[x - 3][y - 1] << (12 - bit)))) ||
-                  ((x <= 946) && ((image[x + 4][y] >> (bit - 4)) || (image[x + 4][y - 1] << (12 - bit)))) ||
-                  checkTheXAksis(image, x, y, 0x80))
-              {
-              }
-              else
-              {
-                count++;
-              }
-            }
-            else
-            {
-              if (((x > 3) && ((image[x - 3][y] << (4 - bit)) || (image[x - 3][y + 1] >> (4 + bit)))) ||
-                  ((x <= 946) && ((image[x + 4][y] << (4 - bit)) || (image[x + 4][y + 1] >> (4 + bit)))) ||
-                  checkTheXAksis(image, x, y, 0x80))
-              {
-              }
-              else
-              {
-                count++;
-              }
-            }
-          }
-          bit--;
-        }
-      }
-    }
-  }
-  return count;
-}
-
 // Function to check if the pixels on x aksis is white
 char checkTheXAksis(unsigned char image[952][119], int x, int y, char bit)
 {
@@ -277,7 +199,6 @@ char checkTheXAksis(unsigned char image[952][119], int x, int y, char bit)
   }
   return 0;
 }
-
 int countCellsBit(unsigned char image[952][119],
                   int count)
 {
@@ -294,9 +215,25 @@ int countCellsBit(unsigned char image[952][119],
         {
           // finds the first true bit, from left to right
           if ((image[x][y] >> bit) & 0x01)
-            if (bit == 4)
+          {
+            // If it is to close to the top
+            if ((y == 0 && bit > 3) && 
+                (((x > 3) && ((image[x - 3][y] & 0x7F) >> (bit - 4))) || 
+                ((x <= 946) && ((image[x + 4][y] & 0x7F) >> (bit - 4))) || 
+                checkTheXAksis(image, x, y, bit)))
             {
-              if (((x > 3) && image[x - 3][y] || (x <= 947) && image[x + 4][y]) || checkTheXAksis(image, x, y, 0x80))
+            }
+            // If it is to close to the bottom
+            else if ((y == 118 && bit < 5) && 
+                (((x > 3) && ((image[x - 3][y] & 0xFE) << (4 - bit))) || 
+                ((x <= 946) && ((image[x + 4][y] & 0xFE) << (4 - bit))) || 
+                checkTheXAksis(image, x, y, bit)))
+            {
+            }
+            // in the middle of the char
+            else if (bit == 4)
+            {
+              if (((x > 3) && image[x - 3][y] || (x <= 946) && image[x + 4][y]) || checkTheXAksis(image, x, y, 0x10))
               {
               }
               else
@@ -311,38 +248,28 @@ int countCellsBit(unsigned char image[952][119],
             else if (bit > 4)
             {
               if (((x > 3) && ((image[x - 3][y] >> (bit - 4)) || (image[x - 3][y - 1] << (12 - bit)))) ||
-                  ((x <= 947) && ((image[x + 4][y] >> (bit - 4)) || (image[x + 4][y - 1] << (12 - bit)))) ||
+                  ((x <= 946) && ((image[x + 4][y] >> (bit - 4)) || (image[x + 4][y - 1] << (12 - bit)))) ||
                   checkTheXAksis(image, x, y, 0x80))
               {
               }
               else
               {
                 count++;
-                for (int i = -3; i <= 4; i++)
-                { // TODO lav noget ordentligt
-                  image[x + i][y] = 0;
-                  image[x + i][y - 1] = 0;
-                }
               }
             }
             else
             {
               if (((x > 3) && ((image[x - 3][y] << (4 - bit)) || (image[x - 3][y + 1] >> (4 + bit)))) ||
-                  ((x <= 947) && ((image[x + 4][y] << (4 - bit)) || (image[x + 4][y + 1] >> (4 + bit)))) ||
+                  ((x <= 946) && ((image[x + 4][y] << (4 - bit)) || (image[x + 4][y + 1] >> (4 + bit)))) ||
                   checkTheXAksis(image, x, y, 0x80))
               {
               }
               else
               {
                 count++;
-                for (int i = -3; i <= 4; i++)
-                { // TODO lav noget ordentligt
-                  image[x + i][y] = 0;
-                  image[x + i][y + 1] = 0;
-                }
               }
             }
-
+          }
           bit--;
         }
       }
@@ -452,6 +379,6 @@ int main(int argc, char **argv)
     }
     // printf("\n");
   }
-
+  printf("%d",otsuThreshold(input_image));
   return 0;
 }
