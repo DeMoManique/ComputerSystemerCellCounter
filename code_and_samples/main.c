4302745f9d7eeb1e7a3cce3e639bf189a862e24a
DeMoManique <frederik.hvarregaard.andersen@gmail.com>
2023-09-25 15:01:52 +0200

    testing stuffs

--- code_and_samples/main.c
+++ code_and_samples/main.c
@@ -17,11 +17,11 @@ clock_t start, end;
 double cpu_time_used;
 
 // Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]; // skal fjernes
-  unsigned char greyImage[BMP_WIDTH][BMP_HEIGTH];
-  unsigned char bitImage[BMP_WIDTH][BIT_WIDTH];
-  unsigned char controlImage[BMP_WIDTH][BIT_WIDTH];
+unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]; // skal fjernes
+unsigned char greyImage[BMP_WIDTH][BMP_HEIGTH];
+unsigned char bitImage[BMP_WIDTH][BIT_WIDTH];
+unsigned char controlImage[BMP_WIDTH][BIT_WIDTH];
 
 void delay(int number_of_seconds)
 {
@@ -32,37 +32,38 @@ void delay(int number_of_seconds)
   clock_t start_time = clock();
 
   // looping till required time is not achieved
-  while (clock() < start_time + milli_seconds);
+  while (clock() < start_time + milli_seconds)
+    ;
 }
 
 // Main function
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  
-  //printf("Reading image \n");
+
+  // printf("Reading image \n");
   read_bitmap(argv[1], input_image);
-  //printf("greyscaling \n");
+  // printf("greyscaling \n");
   imageGreyScaling(input_image, greyImage);
-  //printf("calculating threshold: ");
-  int threshold=otsuThreshold(greyImage);
+  // printf("calculating threshold: ");
+  int threshold = otsuThreshold(greyImage);
   // printf("%d \nconverting to bits\n", threshold);
 
-
   start = clock();
-  imageToBits(greyImage, bitImage, threshold-20);
+  imageToBits(greyImage, bitImage, threshold - 20);
   end = clock();
 
-  //printf("printing image\n");
-  // printBits(bitImage, output_image);
-  // write_bitmap(output_image, argv[2]);
-  //printf("Eroding image\n");
-  
+  // printf("printing image\n");
+  //  printBits(bitImage, output_image);
+  //  write_bitmap(output_image, argv[2]);
+  // printf("Eroding image\n");
+
   int counter = 0;
-  while (erode(bitImage, controlImage,1)) {
-     delay(1);
-     printBits(bitImage, output_image);
-     write_bitmap(output_image, argv[2]);
-    
+  while (erode(bitImage, controlImage, 1))
+  {
+    //  delay(1);
+    //  printBits(bitImage, output_image);
+    //  write_bitmap(output_image, argv[2]);
+
     counter = count(bitImage, counter, input_image);
   }
   write_bitmap(input_image, argv[2]);
