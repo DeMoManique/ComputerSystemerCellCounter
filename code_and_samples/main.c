cdeb3d5e06826bdfcad460db997c3609012c0985
DeMoManique <114985910+DeMoManique@users.noreply.github.com>
2023-09-30 20:11:59 +0200

    spaceTest

--- code_and_samples/main.c
+++ code_and_samples/main.c
@@ -18,7 +18,6 @@ double cpu_time_used;
 
 // Declaring the array to store the image (unsigned char = unsigned 8 bit)
 unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]; // skal fjernes
 union images
 {
   unsigned char greyImage[BMP_WIDTH][BMP_HEIGTH];
@@ -43,75 +42,7 @@ void delay(int number_of_seconds) {
 // Main function
 int main(int argc, char** argv) {
 
-  // reads the iamge
-  read_bitmap(argv[1], input_image);
-  // printf("greyscaling \n");
-  // printf("calculating threshold: ");
-  start = clock();
-  for (int i = 0; i < 1000; i++) {
-    imageGreyScaling(input_image, images.greyImage);
-  }
-  end = clock();
-  double greyTime = (double)(end - start);
-
-  start = clock();
-  int threshold = 0;
-  for (int i = 0; i < 1000; i++) {
-    threshold = otsuThreshold(images.greyImage);
-  }
-  end = clock();
-  double threshTime = (double)(end - start);
-
-
-  // printf("%d \nconverting to bits\n", threshold);
-  start = clock();
-  for (int i = 0; i < 1000; i++) {
-    imageGreyScaling(input_image, images.greyImage);
-    imageToBits(images.greyImage, images.bitImages.bitImage, threshold);
-  }
-  end = clock();
-  double toBitTime = (double)(end - start) - greyTime;
-  int counter = 0;
-  start = clock();
-  for (int i = 0; i < 1000; i++) {
-    counter = 0;
-    imageGreyScaling(input_image, images.greyImage);
-    imageToBits(images.greyImage, images.bitImages.bitImage, threshold);
-    counter = countLarge(images.bitImages.bitImage, counter, input_image);
-    erode(images.bitImages.bitImage, images.bitImages.controlImage, 0);
-    erode(images.bitImages.bitImage, images.bitImages.controlImage, 0);
-    counter = countLarge(images.bitImages.bitImage, counter, input_image);
-    while (erode(images.bitImages.bitImage, images.bitImages.controlImage, 1)) {
-      counter = count(images.bitImages.bitImage, counter, input_image);
-    }
-  }
-  end = clock();
-  double erodePlusCount = (double)(end - start) - greyTime - toBitTime;
-
-  for (int i = 0; i < 1000; i++) {
-    imageGreyScaling(input_image, images.greyImage);
-    imageToBits(images.greyImage, images.bitImages.bitImage, threshold);
-    while(erode(images.bitImages.bitImage, images.bitImages.controlImage, 0));
-  }
-  end = clock();
-  double erodeTime = (double)(end - start) - greyTime - toBitTime;
-
-  for (int i = 0; i < 1000; i++) {
-    imageGreyScaling(input_image, images.greyImage);
-    imageToBits(images.greyImage, images.bitImages.bitImage, threshold);
-    while(erode(images.bitImages.bitImage, images.bitImages.controlImage, 1));
-  }
-  end = clock();
-  double erodeTimeTwo = (double)(end - start) - greyTime - toBitTime;
-
-
-  write_bitmap(input_image, argv[2]);
-  printf("grey time: %f ms\n", greyTime * 1000 / CLOCKS_PER_SEC);
-  printf("Otsu time: %f ms\n", threshTime * 1000 / CLOCKS_PER_SEC);
-  printf("To Bit time: %f ms\n", toBitTime * 1000 / CLOCKS_PER_SEC);
-  printf("Erode + count time: %f ms\n", erodePlusCount * 1000 / CLOCKS_PER_SEC);
-  printf("erode time: %f ms\n", erodeTime * 1000 / CLOCKS_PER_SEC);
-  printf("erode Two time: %f ms\n", erodeTimeTwo * 1000 / CLOCKS_PER_SEC);
+  printf("tvunget plads %d alt plads %d", sizeof(input_image),sizeof(images));
 
   return 0;
 }
